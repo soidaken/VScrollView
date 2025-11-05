7f1a294bbc6cbd37f35446d9c24a8d8788ef56d8
sodia <adioskensunny@gmail.com>
2025-11-05 13:37:37 +0800

    加入一个邮件列表需求案例

---
+++ assets/vscrollview/samples/script/scene8.ts
@@ -0,0 +1,105 @@
+import { _decorator, Color, Component, game, Label, Node, Sprite, SpriteFrame } from 'cc';
+import { VirtualScrollView } from '../../VScrollView';
+import UIButton from './UIButton';
+const { ccclass, property } = _decorator;
+
+@ccclass('scene8')
+export class scene8 extends Component {
+  @property(VirtualScrollView)
+  vlist: VirtualScrollView | null = null;
+
+  //列表数据
+  private data: any[] = [];
+
+  private renderOptOnOff = false;
+
+  onLoad() {
+    game.frameRate = 120;
+    // 模拟数据
+    for (let i = 0; i < 20; i++) {
+      this.data.push({
+        data1: `邮件标题${i + 1}`,
+        data2: `2025.10.${1 + i}`,
+        clicked :false
+      });
+    }
+
+    // 设置虚拟列表数据
+    if (this.vlist) {
+      this.vlist.renderItemFn = (itemNode: Node, index: number) => {
+        const title = itemNode.getChildByName('title').getComponent(Label);
+        const time = itemNode.getChildByName('time').getComponent(Label);
+        title!.string = this.data[index].data1;
+        time!.string = this.data[index].data2;
+
+        //子项中单独的button处理,没有什么特别的
+        const btnsure = itemNode.getChildByName('btn');
+        UIButton.onClicked(btnsure, (button: UIButton) => {
+          const tip = this.node.getChildByName('tip').getComponent(Label);
+          tip.string = `你点击了第${index + 1}项,内容:${this.data[index].data1}`;
+        });
+
+        if(this.data[index].clicked){
+          title!.color = new Color(255,0,0);
+        }else{
+          title!.color = new Color(75,75,75);
+        }
+        //用来控制子项中按钮的点击事件是否同时影响上层节点的交互
+        // btnsure.getComponent(UIButton).b_stopPropagation = false;
+      };
+
+      //如果设置了子项点击回调,则会自动开启子项点击效果
+      this.vlist.onItemClickFn = (itemNode: Node, index: number) => {
+        const tip = this.node.getChildByName('tip').getComponent(Label);
+        tip.string = `邮件内容:${this.data[index].data1}`;
+        this.data.forEach((element, idx) => {
+          if(index === idx){
+            element.clicked = true;
+          }else{
+            element.clicked = false;
+          }
+        });
+
+        this.vlist.refreshList(this.data);
+      }
+        
+      };
+
+      this.vlist.refreshList(this.data);
+
+      // this.vlist.onOffSortLayer(this.renderOptOnOff);
+    
+
+    UIButton.onClicked(this.node.getChildByName('btn1'), (button: UIButton) => {
+      this.data[1].data1 = '【已修改】重要通知2';
+      this.vlist.refreshIndex(1);
+    });
+
+    UIButton.onClicked(this.node.getChildByName('btn2'), (button: UIButton) => {
+      this.vlist.scrollToBottom(true);
+    });
+
+    UIButton.onClicked(this.node.getChildByName('btn3'), (button: UIButton) => {
+      this.vlist.scrollToIndex(10 - 1, true);
+    });
+
+    UIButton.onClicked(this.node.getChildByName('btn4'), (button: UIButton) => {
+      this.renderOptOnOff = !this.renderOptOnOff;
+      const tip = this.node.getChildByName('tip').getComponent(Label);
+      tip.string = `分层优化:${this.renderOptOnOff ? '开启' : '关闭'}`;
+      this.vlist.onOffSortLayer(this.renderOptOnOff);
+    });
+
+    UIButton.onClicked(this.node.getChildByName('btn5'), (button: UIButton) => {
+      this.data.push({
+        data1: `新增的数据 ${this.data.length + 1}`,
+        data2: `2025.10.${this.data.length + 1}`,
+      });
+
+      //有时候,列表在顶部,你要新增一项,这里就是先设置列表跳到旧的底部,再刷新滚动到新的底部,这就很自然.
+      this.vlist.flashToBottom();
+      this.vlist.refreshList(this.data);
+      this.vlist.scrollToBottom(true);
+    });
+  }
+}
